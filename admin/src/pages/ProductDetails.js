7ee2d8d5e213c3a94419cee73005e099fd1128ff
tewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2023-09-20 20:24:14 +0300

    Update ProductDetails.js

--- admin/src/pages/ProductDetails.js
+++ admin/src/pages/ProductDetails.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BsUpload} from 'react-icons/bs'
 
 const ProductDetails = () => {
+    const [images, setImages] = useState([])
     const handleSubmit = (e) => {
-        
         console.log('submitted')
     }
+
+    const handleImage = (e) => {
+        setImages([...images,e.target.files[0]])
+    }
+
     return (
         <div>
             <h1 className='font-semibold'> Product Details </h1>
@@ -19,11 +24,19 @@ const ProductDetails = () => {
                 <label> Storage(GB) </label>
                 <input type="text" className='product-input'/>
                 <label> Photos </label>
-                <label className='upload-btn'>
-                    <BsUpload size={20}/>
-                    Add Image
-                    <input className='absolute h-full w-full hidden' type='file'/>
-                </label>
+                <div className='flex gap-3'>
+                    {
+                        images.length > 0 &&
+                        images.map( image => {
+                            return <img className='w-28 h-28 border border-zinc-400 rounded-md' src={URL.createObjectURL(image)} alt="product" />
+                        })
+                    }
+                    <label className='upload-btn'>
+                        <BsUpload size={20}/>
+                        Add Image
+                        <input className='absolute h-full w-full hidden' type='file' onChange={handleImage}/>
+                    </label>
+                </div>
                 <label> Description </label>
                 <textarea name="desc" cols="30" rows="10" className='product-input'></textarea>
             </form>
