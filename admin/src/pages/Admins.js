1698a00fb701ac703ad2efb58b3be97123d90460
tewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2023-09-25 21:35:03 +0300

    Update Admins.js

--- admin/src/pages/Admins.js
+++ admin/src/pages/Admins.js
@@ -8,7 +8,6 @@ const Admins = () => {
       <div className='flex flex-col mt-8 font-medium text-sm'>
         Add new admin
         <div className='flex justify-center gap-6 mt-3 mr-3'>
-          <input type="text" className='product-input h-10 w-full ' placeholder='google email'/>
           <button className='bg-blue-700 w-40 p-2 h-10 text-white rounded-sm'>Add Admin</button>
         </div>
       </div>
