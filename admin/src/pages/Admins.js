7c55786c4bfab6637523e305501554173e167a6c
tewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2023-09-26 20:54:27 +0300

    Updated Admin page

--- admin/src/pages/Admins.js
+++ admin/src/pages/Admins.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import AdminDetails from '../components/AdminDetails'
+import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
 
 const Admins = () => {
+  // React hooks
+  const googleRef = useRef()
+  
+  // Functions
+
+  const login = useGoogleLogin({
+    onSuccess: async tokenResponse => {
+      try{
+        const data = await fetch("https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers:{
+              "Authorization": `${tokenResponse.token_type} ${tokenResponse.access_token}` 
+            }
+          }
+        )
+        console.log(data)
+      }catch(error){
+        console.log(error)
+      }
+    }
+  });
+  
   return (
     <div className='font-semibold'>
       Admins
       <div className='flex flex-col mt-8 font-medium text-sm'>
         Add new admin
-        <div className='flex justify-center gap-6 mt-3 mr-3'>
-          <button className='bg-blue-700 w-40 p-2 h-10 text-white rounded-sm'>Add Admin</button>
+        <div className='flex gap-6 mt-3 mr-3'>
+          <button onClick={() => login()} className='bg-blue-700 w-40 p-2 h-10 text-white rounded-sm'>Add Admin</button>
         </div>
       </div>
       <div className='flex flex-col mt-4 font-medium text-sm'>
