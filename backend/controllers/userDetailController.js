088a7a0b82841e855412f78d1c476da43738a721
tewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2023-11-29 02:53:00 +0300

    Update userDetailController.js

--- backend/controllers/userDetailController.js
+++ backend/controllers/userDetailController.js
@@ -13,10 +13,8 @@ const getUserDetails = async(req,res) =>{
 
 const createUserDetails = async(req,res) =>{
     const {user_id, username, email} = req.body
-    console.log(user_id, username, email)
     try{
         const userDetails = await UserDetail.create({user_id,username, email})
-        console.log(userDetails)
         res.status(200).json(userDetails)
     }catch(e){
         console.log(e)
